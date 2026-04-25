57e94e5d4dccda11ae8a4f0c76f4beb55e3a0fdf
syed-shah5 <iftekharshah@hotmail.com>
2026-04-25 10:36:34 +0100

    Add CSRF_TRUSTED_ORIGINS for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,8 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
+CSRF_TRUSTED_ORIGINS = ['https://shallion-dem-app.onrender.com']
+
 
 # Application definition
 
